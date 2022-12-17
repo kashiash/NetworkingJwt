7677f310534aaf7ba294594b43b6cf04748e71d2
Jacek Kosinski <kashiash@gmail.com>
2022-12-17 20:28:43 +0100

    Initial Commit

---
+++ NetworkinJwt/NetworkinJwtApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkinJwtApp.swift
+//  NetworkinJwt
+//
+//  Created by Jacek Kosiński G on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkinJwtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
